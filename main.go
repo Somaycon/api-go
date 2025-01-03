d2b99ba0c755dee03f58982b6321c3c01ac12dfc
somaycon <contato.somaycon@gmail.com>
2025-01-03 12:11:29 -0300

    feat: Add Custom logger

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/Somaycon/api-go/router"
+import (
+	"github.com/Somaycon/api-go/config"
+	"github.com/Somaycon/api-go/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errof("Config initialization error: %v", err)
+		return
+	}
 	router.Initialize()
 }
