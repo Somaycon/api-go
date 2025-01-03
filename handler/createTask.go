052da9eb4d68a2d59e6867ffa4e8f024f0fd2adf
somaycon <contato.somaycon@gmail.com>
2025-01-03 15:12:14 -0300

    feat: add create task logic

--- handler/createTask.go
+++ handler/createTask.go
@@ -3,11 +3,29 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Somaycon/api-go/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateTaskHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST task",
-	})
+	request := CreateTaskRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errof("validation error: %v", err.Error())
+		SendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+	task := schemas.Task{
+		Name:        request.Name,
+		Description: request.Description,
+	}
+
+	if err := db.Create(&task).Error; err != nil {
+		logger.Errof("error creating task: %v", err.Error())
+		SendError(ctx, http.StatusInternalServerError, "error creating task on database")
+		return
+	}
+	SendSucess(ctx, "create-task", task)
 }
