5b4a3081be416d530573f823976c368008511194
somaycon <contato.somaycon@gmail.com>
2025-01-03 13:55:33 -0300

    feat: add database migration and handler

---
+++ config/sqlite.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"os"
+
+	"github.com/Somaycon/api-go/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := "./db/main.db"
+	_, err := os.Stat(dbPath)
+
+	if os.IsNotExist(err) {
+		logger.Info("database file not found, creating...")
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+	}
+
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Errof("sqlite openning error: %v", err)
+		return nil, err
+	}
+	err = db.AutoMigrate(&schemas.Task{})
+	if err != nil {
+		logger.Errof("sqlite automigration error %v", err)
+		return nil, err
+	}
+	return db, nil
+}
--- schemas/task.go
+++ schemas/task.go
@@ -1,12 +1,22 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Task struct {
 	gorm.Model
-	Id          int
 	Name        string
 	Description string
+}
+
+type TaskResponse struct {
+	Id          uint      `json:"id"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+	DeletedAt   time.Time `json:"deletedAt, omitempty"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
 }
