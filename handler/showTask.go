be1fd2b2c68a01b8ff58e7009a8b59af89698cca
somaycon <contato.somaycon@gmail.com>
2025-01-04 09:53:39 -0300

    fix: Change show task logic error where id not exist

--- handler/showTask.go
+++ handler/showTask.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/Somaycon/api-go/schemas"
 	"github.com/gin-gonic/gin"
@@ -13,8 +14,13 @@ func ShowTaskHandler(ctx *gin.Context) {
 		SendError(ctx, http.StatusInternalServerError, errParamIsRequired("id", "queryParameter").Error())
 		return
 	}
+	taskId, err := strconv.Atoi(id)
+	if err != nil {
+		SendError(ctx, http.StatusBadRequest, "invalid format")
+		return
+	}
 	task := schemas.Task{}
-	if err := db.First(&task).Error; err != nil {
+	if err := db.Where("id=?", taskId).First(&task).Error; err != nil {
 		SendError(ctx, http.StatusNotFound, "task not found")
 		return
 	}
