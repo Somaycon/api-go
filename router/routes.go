f8817f59fc08bf59c295eed5975c4e87a72351b0
somaycon <contato.somaycon@gmail.com>
2025-01-04 08:48:22 -0300

    feat: User Routes

--- router/routes.go
+++ router/routes.go
@@ -9,10 +9,18 @@ func initializeRoutes(router *gin.Engine) {
 	handler.InitializeHandler()
 	v1 := router.Group("/api/")
 	{
+		// Task
 		v1.GET("/task", handler.ShowTaskHandler)
 		v1.POST("/task", handler.CreateTaskHandler)
 		v1.DELETE("/task", handler.DeleteTaskHandler)
 		v1.PUT("/task", handler.UpdateTaskHandler)
 		v1.GET("/tasks", handler.ShowAllTasksHandler)
+
+		// User
+		v1.GET("/user")
+		v1.GET("/users")
+		v1.POST("user")
+		v1.PUT("/user")
+		v1.DELETE("/user")
 	}
 }
