13b8ce497d3cfc188383ac56495292492de3ac86
somaycon <contato.somaycon@gmail.com>
2025-01-07 09:57:56 -0300

    feat: Get the user id on token

--- handler/showUser.go
+++ handler/showUser.go
@@ -2,23 +2,18 @@ package handler
 
 import (
 	"net/http"
-	"strconv"
 
 	"github.com/Somaycon/api-go/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowUserHandler(ctx *gin.Context) {
-	id := ctx.Query("id")
-	if id == "" {
-		SendError(ctx, http.StatusInternalServerError, errParamIsRequired("id", "queryParameter").Error())
+	userId, exists := ctx.Get("userId")
+	if !exists {
+		SendError(ctx, http.StatusUnauthorized, "User not authenticated")
 		return
 	}
-	userId, err := strconv.Atoi(id)
-	if err != nil {
-		SendError(ctx, http.StatusBadRequest, "invalid format")
-		return
-	}
+
 	user := schemas.User{}
 	if err := db.Where("id=?", userId).First(&user).Error; err != nil {
 		SendError(ctx, http.StatusNotFound, "user not found")
